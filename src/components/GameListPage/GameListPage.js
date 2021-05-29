3794d077d078cb323a93e0aadf6b8560449cd084
chohyeryung <s2019w38@e-mirim.hs.kr>
2021-05-30 02:38:39 +0900

    Add get img json

--- src/components/GameListPage/GameListPage.js
+++ src/components/GameListPage/GameListPage.js
@@ -1,9 +1,51 @@
 import React from "react";
 
+import "../../css/game_list.css";
+import { ImageData } from "../../data/ImageData";
+
+// const goodsImages = [
+//     goodsImage1,
+//     goodsImage2,
+//     goodsImage3,
+//     goodsImage4,
+//     goodsImage5,
+//     goodsImage6,
+//     goodsImage7,
+//     goodsImage8,
+// ];
+
 export default function GameListPage() {
 
     return(
-        <h1>hihi</h1>
+        <div id={"Container"}>
+            <table className={"ImageTable"}>
+                {/* <tr>
+                {goodsImages.map((image, index) => {
+                    return index < 4 ?
+                    <td>
+                        <img src={image} className={"GoodsImage"} />
+                    </td> : ''
+                })}
+                </tr>
+                <tr>
+                {goodsImages.map((image, index) => {
+                    return index >= 4 ?
+                    <td>
+                        <img src={image} className={"GoodsImage"} />
+                    </td> : ''
+                })}
+                </tr> */}
+                <tr>
+                    {ImageData.map((data) => {
+                        return (
+                            <td>
+                                <img src={data.imgsrc} className={"GoodsImage"} />
+                            </td>
+                        )
+                    })}
+                </tr>
+            </table>
+        </div>
         
     )
 }
---
+++ src/data/ImageData.js
@@ -0,0 +1,43 @@
+import goodsImage1 from "../assets/images/1.jpeg";
+import goodsImage2 from "../assets/images/2.png";
+import goodsImage3 from "../assets/images/3.jpeg";
+import goodsImage4 from "../assets/images/4.jpeg";
+import goodsImage5 from "../assets/images/5.png";
+import goodsImage6 from "../assets/images/6.jpg";
+import goodsImage7 from "../assets/images/7.png";
+import goodsImage8 from "../assets/images/8.jpg";
+
+export const ImageData = [
+    {
+        id : 1,
+        imgsrc : goodsImage1
+    },
+    {
+        id : 2,
+        "imgsrc" : goodsImage2
+    },
+    {
+        id : 3,
+        "imgsrc" : goodsImage3
+    },
+    {
+        id : 4,
+        "imgsrc" : goodsImage4
+    },
+    {
+        id : 5,
+        "imgsrc" : goodsImage5
+    },
+    {
+        id : 6,
+        "imgsrc" : goodsImage6
+    },
+    {
+        id : 7,
+        "imgsrc" : goodsImage7
+    },
+    {
+        id : 8,
+        "imgsrc" : goodsImage8
+    }
+]
