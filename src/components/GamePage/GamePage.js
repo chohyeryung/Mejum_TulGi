74e2d0e2510d7b2ecf121b989f1291c72ad9e647
s2019w33 <s2019w33@e-mirim.hs.kr>
2021-05-29 21:45:50 +0900

    show webcam and get result from model

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -1,72 +1,55 @@
-import React, { useEffect, useState } from "react";
-import Webcam from "react-webcam";
+import React, { useEffect, useRef, useState } from "react";
+import ml5 from 'ml5';
+import useInterval from '@use-it/interval';
+
+let classifier;
 
 export default function GamePage() {
-    const [time, setTime] = useState(0);
-    let model, maxPredictions, labelContainer;
 
-    const videoConstraints = {
-        width: 1000,
-        height: 720,
-        facingMode: "user"
-    };
+  const videoRef = useRef();
+  const [result, setResult] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [time, setTime] = useState(0);
 
-    useEffect(() => {
-        const script1 = document.createElement("script");
-        script1.src = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@1.3.1/dist/tf.min.js";
-        script1.async = true;
-        document.body.appendChild(script1);
+  useEffect(() => {
 
-        const script2 = document.createElement("script");
-        script2.src = "https://cdn.jsdelivr.net/npm/@teachablemachine/image@0.8/dist/teachablemachine-image.min.js";
-        script2.async = true;
-        document.body.appendChild(script2);
+    classifier = ml5.imageClassifier("./model/model.json", () => {
+      navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+        setLoaded(true);
+      })
+    })
+  }, []);
 
-        init().then(() => {
-            predict();
-        })
-        return setTimeout(() => setTime(time+1), 1000);
-
-    }, [time]);
-
-    async function init() {
-        const modelURL = "../../my_model/model.json";
-        const metadataURL = "../../my_model/metadata.json";
-
-        model = await tmImage.load(modelURL, metadataURL);
-        maxPredictions = model.getTotalClasses();
-    
-    
-        document.getElementById("webcam-container").appendChild(webcam.canvas);
-        labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) {
-          // and class labels
-          labelContainer.appendChild(document.createElement("div"));
+  // 0.5초마다 분석
+  useInterval(() => {
+    if(classifier) {
+      classifier.classify(videoRef.current, (error, results) => {
+        if(error) {
+          console.log(error);
+          return;
         }
+        setResult(results[0]);
+        console.log(results[0]);
+      });
     }
+  }, 500);
 
-    async function predict() {
-        // predict can take in an image, video or canvas html element
-        const prediction = await model.predict(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) {
-          const classPrediction =
-            prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-          labelContainer.childNodes[i].innerHTML = classPrediction;
-        }
-      }
-
+  return(
+    <div style={{ display:'flex', flexDirection:'column', justifyContent:'center' }}>
+      <div style={{ marginTop:'60px' }}>
+        <video
+          style={{ justifyContent:'center', transform: "scale(-1,1)" }}
+          ref={videoRef}
+          width="1000"
+          height="500"
+        />
+      </div>
+      <div style={{ display:'flex', justifyContent:'center' }}>{ result.label }: { result.confidence }</div>
+    </div>
     
-    return (
-        <>
-            <Webcam
-            audio={false}
-            height={720}
-            width={1000}
-            videoConstraints={videoConstraints}
-            />
-
-            <h3>{time}</h3>
-        </>
-
-    )
+  )
 }
