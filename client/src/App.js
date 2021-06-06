a69bf1087971c042566758f3abb589ae397177c2
jimin-oh <wlals6828@daum.net>
2021-06-06 15:31:38 +0900

    game_end

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import GameListPage from "./components/GameListPage/GameListPage";
 import GameEnd from "./components/GameEndPage/GameEndPage"
+import GameEndRank from "./components/GameEndRankPage/GameEndRankPage"
+
 import GameLandingPage from "./components/GameLandingPage";
 import MainPage from "./components/MainPage/MainPage";
 import HowToUsePage from "./components/HowToUsePage/HowToUsePage";
-import GameRanking from "./components/GameRankingPage/GameRankingPage";
+import GameRanking from "./components/GameEndRankPage/GameEndRankPage";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/game" component={GameLandingPage} />
         <Route path="/game_list" component={GameListPage} />
         <Route path="/game_end" component={GameEnd} />
+        <Route path="/game_end_name" component={GameEndRank} />
+
         <Route path="/game_ranking" component={GameRanking} />
         <Route path="/" component={MainPage} />
         <Route path="/how_to_use" component={HowToUsePage} />
