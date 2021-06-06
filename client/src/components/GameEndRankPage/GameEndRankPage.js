1782c6c7ca1d1d17248c0b585840145f618bc0ee
jimin-oh <wlals6828@daum.net>
2021-06-06 17:14:36 +0900

    center fix

--- client/src/components/GameEndRankPage/GameEndRankPage.js
+++ client/src/components/GameEndRankPage/GameEndRankPage.js
@@ -12,21 +12,26 @@ function GameEndRankPage(){
   const history = useHistory();
   const ButtonClick = () => {
 
-    axios.post('http://localhost:5000/score',{
-      name : name,
-      score :time
-    })
-    .then((res)=>{
-      console.log(res)
-    })
-    .catch((err)=>{
-      console.log(err)
-    })
-
-
-    history.push({
-      pathname: "/how-to-use",
-    })
+    if(name == ""){
+      alert("이름을 입력해주세요")
+    }else{
+      axios.post('http://localhost:5000/score',{
+        name : name,
+        score :time
+      })
+      .then((res)=>{
+        console.log(res)
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+  
+  
+      history.push({
+        pathname: "/how-to-use",
+      })
+    }
+   
   }
 
   const handleChange =(e) =>{
@@ -38,9 +43,10 @@ function GameEndRankPage(){
             <div className = 'container'>
                 <div className ="gameover_text">Game Over</div>
                 <div className ="time_text">{time}초</div>
-
-                <input type="text" placeholder="이름을 입력해주세요" className="input_name" onChange={handleChange}/>
-                <img src={check} className="check" onClick={ButtonClick} />
+                <div className="rank_name">
+                  <input type="text" placeholder="이름을 입력해주세요" className="input_name" onChange={handleChange}/>
+                  <img src={check} className="check" onClick={ButtonClick} />
+                </div>
                 <Link to={{ pathname: "/how-to-use" }}>
                   <div className="go_game">랭킹 참여안하기</div>
                 </Link> 
