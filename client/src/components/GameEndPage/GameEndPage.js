734f2e392c8e3424b55fb2142b818ed9cf64d70a
jimin-oh <wlals6828@daum.net>
2021-06-08 13:36:58 +0900

    mysql, css  change

--- client/src/components/GameEndPage/GameEndPage.js
+++ client/src/components/GameEndPage/GameEndPage.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
 import "./GameEndPage.css";
 import { Link, useHistory } from "react-router-dom";
+import Boss from "./Boss.png";
 
 function GameEndRankPage(){
   const history = useHistory();
@@ -9,9 +10,10 @@ function GameEndRankPage(){
       <div id ={"divContainer"}>
             <div className = "round">      </div>
         
-            <div className = 'container'>
+            <div className = 'text'>
                 <div className ="gameover_text">Game Over</div>
-                <button onClick={() =>history.push({pathname: "/how-to-use"})} className="end">완료</button>
+                <button onClick={() =>history.push({pathname: "/"})} className="end">완료</button>
+                <img src = {Boss} className = "photo"></img>
             </div>
       </div>
   );
