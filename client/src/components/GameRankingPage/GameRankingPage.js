537a647fc395d9f552533adca155ad0ff83e6d3a
yiseungmin <s2019w33@e-mirim.hs.kr>
2021-06-22 11:15:12 +0900

    game ranking background color setting

--- client/src/components/GameRankingPage/GameRankingPage.js
+++ client/src/components/GameRankingPage/GameRankingPage.js
@@ -41,92 +41,94 @@ function GameRanking() {
     );
 
   return (
-    <div className="container">
-      <div className="medalImage">
-        <div className="gameRetry">
-          <a href="/" style={{ textDecoration: "none", color: "blue" }}>
-            다시하기
-          </a>
+    <div style={{ backgroundColor: 'rgb(224, 224, 224)', width: '100%', height: '100vh' }}>
+      <div className="container">
+        <div className="medalImage">
+          <div className="gameRetry">
+            <a href="/" style={{ textDecoration: "none", color: "blue" }}>
+              다시하기
+            </a>
+          </div>
+          <div className="bronzeScore">
+            <img src={silvermedal} width="250" height="310" alt="silver medal" />
+            {score.map((s, index) => {
+              if (index === 1) {
+                return (
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span style={{ textAlign: "center", fontWeight: "bold" }}>
+                      {s.name}
+                    </span>
+                    <span style={{ textAlign: "center" }}>{s.score}s</span>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="goldScore">
+            <img src={goldmedal} width="300" height="410" alt="gold medal" />
+            {score.map((s, index) => {
+              if (index === 0) {
+                return (
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span style={{ textAlign: "center", fontWeight: "bold" }}>
+                      {s.name}
+                    </span>
+                    <span style={{ textAlign: "center" }}>{s.score}s</span>
+                  </div>
+                );
+              }
+            })}
+          </div>
+          <div className="bronzeScore">
+            <img src={bronzemedal} width="240" height="310" alt="bronze medal" />
+            {score.map((s, index) => {
+              if (index === 2) {
+                return (
+                  <div
+                    style={{
+                      display: "flex",
+                      flexDirection: "column",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span style={{ textAlign: "center", fontWeight: "bold" }}>
+                      {s.name}
+                    </span>
+                    <span style={{ textAlign: "center" }}>{s.score}s</span>
+                  </div>
+                );
+              }
+            })}
+          </div>
         </div>
-        <div className="bronzeScore">
-          <img src={silvermedal} width="250" height="310" alt="silver medal" />
-          {score.map((s, index) => {
-            if (index === 1) {
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                  }}
-                >
-                  <span style={{ textAlign: "center", fontWeight: "bold" }}>
-                    {s.name}
-                  </span>
-                  <span style={{ textAlign: "center" }}>{s.score}s</span>
-                </div>
-              );
-            }
-          })}
-        </div>
-        <div className="goldScore">
-          <img src={goldmedal} width="300" height="410" alt="gold medal" />
-          {score.map((s, index) => {
-            if (index === 0) {
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                  }}
-                >
-                  <span style={{ textAlign: "center", fontWeight: "bold" }}>
-                    {s.name}
-                  </span>
-                  <span style={{ textAlign: "center" }}>{s.score}s</span>
-                </div>
-              );
-            }
-          })}
-        </div>
-        <div className="bronzeScore">
-          <img src={bronzemedal} width="240" height="310" alt="bronze medal" />
-          {score.map((s, index) => {
-            if (index === 2) {
-              return (
-                <div
-                  style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                  }}
-                >
-                  <span style={{ textAlign: "center", fontWeight: "bold" }}>
-                    {s.name}
-                  </span>
-                  <span style={{ textAlign: "center" }}>{s.score}s</span>
-                </div>
-              );
-            }
-          })}
-        </div>
-      </div>
 
-      <div className="leastContainer">
-        {score.map((s, index) => {
-          if (index >= 3) {
-            return (
-              <div key={s.name} className="restRanking">
-                <div className="attribute">
-                  <div className="index">&nbsp;&nbsp;&nbsp;{index + 1}</div>
-                  <div className="name">{s.name}</div>
-                  <div className="score">{s.score}s&nbsp;&nbsp;</div>
+        <div className="leastContainer">
+          {score.map((s, index) => {
+            if (index >= 3) {
+              return (
+                <div key={s.name} className="restRanking">
+                  <div className="attribute">
+                    <div className="index">&nbsp;&nbsp;&nbsp;{index + 1}</div>
+                    <div className="name">{s.name}</div>
+                    <div className="score">{s.score}s&nbsp;&nbsp;</div>
+                  </div>
                 </div>
-              </div>
-            );
-          }
-        })}
+              );
+            }
+          })}
+        </div>
       </div>
     </div>
   );
